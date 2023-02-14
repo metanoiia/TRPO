ff3e4fe363deb04d21fa187983af59b622f23579
Кирилл Алямовский <61751760+metanoiia@users.noreply.github.com>
2023-02-14 16:16:40 +0300

    lab2 added
    pozor4ik

---
+++ lab2/lab2/main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <sstream>
+
+bool isPalindrome( std::string s )
+{
+    for( size_t i = 0; i < s.length() / 2; i++ )
+    {
+        if( s.at( i ) != s.at( s.length() - 1 - i ) )
+            return false;
+    }
+    return true;
+}
+
+void task1()
+{
+    typedef  std::vector < int > vector;
+
+    std::cout << "Task 1:" << std::endl;
+
+    vector v = { 1, 2, 5, 12, 46, -2, -28, 256, 3, 28 };
+
+    std::cout << "vector before: ";
+    for( int n : v )
+        std::cout << n << " ";
+    std::cout << std::endl;
+
+    int sumMax = 0;
+    int sumMin = 0;
+
+    vector::iterator itMax = std::max_element( v.begin(), v.end() );
+    vector::iterator itMin = std::min_element( v.begin(), v.end() );
+
+    if( itMax != v.end() )
+       sumMax = std::accumulate( v.begin(), itMax,  0 );
+
+    if( itMin != v.end() )
+        sumMin = std::accumulate( itMin + 1, v.end(), 0 );
+
+    *itMax = sumMax;
+    *itMin = sumMin;
+
+    std::cout << "vector after: ";
+    for( int n : v )
+        std::cout << n << " ";
+    std::cout << std::endl ;
+}
+
+void task2( )
+{
+    std::cout << std::endl<< "Task 2:" << std::endl;
+
+    std::string s = "One and two and three and four and five and abba and aaaa and heh and lol";
+    std::string andStr = " and ";
+
+    std::istringstream ss( s );
+    std::string word;
+    size_t num = 0;
+
+    while( std::getline( ss, word, ' ' ) )
+        if( isPalindrome( word ) )
+            num++;
+
+    std::cout << "quantity of palindromes: " << num << std:: endl;
+
+    int pos = -1;
+
+    std::cout << "string before: " << s << std::endl;
+
+    while( ( pos = s.find( andStr ) ) != -1 )
+        s.replace( pos, andStr.length(), ", " );
+
+    std::cout << "string after: " << s << std::endl;
+
+}
+
+int main()
+{
+    task1();
+    task2();
+    return 0;
+}
